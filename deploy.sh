33a3c83526f3248a132cc6df389555ad1d0f0e60
Michael Smith <m@michaelsmith.be>
2024-04-26 21:29:40 +0200

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 DOCKER_DEFAULT_PLATFORM=linux/amd64 docker build -t michaelshmitty/cassandra .
 docker image push michaelshmitty/cassandra
-docker-compose -H "ssh://root@voidwarranties" pull
-docker-compose -H "ssh://root@voidwarranties" up --force-recreate -d
-docker -H "ssh://root@voidwarranties" image prune -f
+docker-compose -H "ssh://voidwarranties" pull
+docker-compose -H "ssh://voidwarranties" up --force-recreate -d
+docker -H "ssh://voidwarranties" image prune -f
